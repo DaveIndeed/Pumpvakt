a0e9dd5d55ff97543cfd1ba5855b069fa0023d71
David Sparrman <David.Sparrman@gmail.com>
2017-10-12 22:07:00 +0200

    modified:   Arduinocomm.py

--- core/Arduinocomm.py
+++ core/Arduinocomm.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import serial
 import logging
@@ -21,7 +22,7 @@ class Ardiunocomm:
 		if not allOk:
 			sys.exit()
 		self.logger.debug("Serial port opened")
-		
+
 
 	def read(self, timeout=10):
 
@@ -36,37 +37,48 @@ class Ardiunocomm:
 			self.logger.debug(readable)
 			return readable
 
-		
+
 	def write(self, message):
 		self.logger.debug("Writing to serial")
 		self.ser.write(bytes(message, 'utf-8'))
 
 	def tandLed(self, lednr):
-		self.write("led %d on" % lednr)
-	
+		msg = "led {0} on\n".format(lednr)
+		self.write(msg)
+
 	def slackLed(self, lednr):
-		self.write("led %d off" % lednr)
-		
+		msg = "led {0} off\n".format(lednr)
+		self.write(msg)
+
+
+def skriv(a, msgout):
+	print("Skriver till serieport")
+	print(" ---> " + msgout)
+	a.write(msgout)
+
+def las(a, timeout=10):
+	print("Läser från serieport")
+	msgin = a.read(timeout)
+	print(" <--- " + msgin)
 
 if __name__ == '__main__':
 	print("Öppnar serieport")
 	a = Ardiunocomm()
-	print("Läser från serieport")
-	msgin = a.read(10)
-	print(" <-- " + msgin)
-	print("Skriver till serieport")
-	msgout = "slfsdkfdjslsdkf";
-	print(" --> " + msgout)
-	a.write(msgout)
-	msgin=a.read()
-	print(" <-- " + msgin)
-	print("Skriver till serieport")
-	msgout = "gettime";
-	print(" --> " + msgout)
-	a.write(msgout)
-	msgin=a.read()
-	print(" <-- " + msgin)
-	a.tandLed(1)
+	msgin = las(a)
+	skriv(a, "sldfsdjkf")
+	msgin = las(a)
+	skriv(a, "gettime")
+	msgin=las(a)
+	skriv(a, "debug")
+	skriv(a, "led 2 on")
+	#skriv(a, "led {0} on\n".format(1))
+	msg=las(a, 1)
 	sleep(1)
-	a.slackLed(1)
-	
+	skriv(a, "led 2 off")
+	#skriv(a, "led {0} off\n".format(1))
+	#sleep(1)
+	#msgin=las(a, 1)
+	#a.tandLed(1)
+	#sleep(1)
+	#a.slackLed(1)
+
